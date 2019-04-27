4c29ee6f73aacfaf0d300d8603900e7d30498cfc
alicanyilmazz <alicanyilmazz@users.noreply.github.com>
2019-04-27 22:58:35 +0300

    Add project files.

---
+++ Jquery.Get.Ajax_using/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Jquery.Get.Ajax_using.Controllers
+{
+    public class HomeController : Controller
+    {
+        private static List<string> my_list = new List<string> { "kadir", "murat", "baseren", "kemal", "sunal", "haktan", "yilmaz" }; //Normalde database den cekeceksin biz göstermek amaçlı böyle yaptık!
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public PartialViewResult Data_Getter(string sending_data = "")
+        {   
+            if(string.IsNullOrEmpty(sending_data) ==false)
+            {
+                my_list.Add(sending_data);
+            }
+
+            //veriler database den cekilir.
+            System.Threading.Thread.Sleep(3000);
+
+            return PartialView("_DataItemPartial",my_list);
+        }
+
+        //public PartialViewResult Data_Getter2(string data)
+        //{
+        //    my_list.Add(data);
+        //    //veriler database den cekilir.
+        //    System.Threading.Thread.Sleep(3000);
+
+        //    return PartialView("_DataItemPartial", my_list);
+        //}
+    }
+}
---
+++ Jquery.Get.Ajax_using/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Jquery.Get.Ajax_using.MvcApplication" Language="C#" %>
